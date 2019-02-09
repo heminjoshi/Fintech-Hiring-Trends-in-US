0e78a7129dcb5ca4223d45963df98864554b53b6
Juhi Pareek <pareek.j@husky.neu.edu>
2019-02-09 00:19:04 -0500

    Datasets, keywords, Algorithms

--- code/README.md
+++
@@ -1,1 +0,0 @@
-# Fintech-Hiring-Trends-in-US
--- code/json2csv.py
+++
@@ -1,9 +0,0 @@
-import json
-import csv
-with open('results.json', 'r') as f:
-	data = json.load(f)
-		
-with open("data.csv", "w") as file:
-    csv_file = csv.writer(file)
-    for item in data:
-        csv_file.writerow([item['jobID'], item['link']] + item['jobT'] + item['jobD'].values())
